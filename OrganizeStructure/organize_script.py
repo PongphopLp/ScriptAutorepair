7fd821e81980d39510bf4fa842d0b67c8b3edeed
PongphopLp <poohcheet@gmail.com>
2023-06-13 15:07:25 +0900

    Create organize script and rename method script.

---
+++ OrganizeStructure/organize_script.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+# Source directory
+source_directory = 'data'
+
+# Destination directory
+destination_directory = 'output'
+
+# Iterate over the pairs and methods
+for pair_number in range(1, 1343):
+    for method_number in range(1, 3):
+        # Create the source and destination paths
+        source_path = os.path.join(source_directory, f'Pair{pair_number}', f'Method{method_number}')
+        destination_path = os.path.join(destination_directory, f'Pair{pair_number}', f'Pair{pair_number}_Method{method_number}', 'test')
+
+        # Create the destination directory if it doesn't exist
+        if not os.path.exists(destination_path):
+            os.makedirs(destination_path)
+
+        # Move the files
+        source_file1 = os.path.join(source_path, 'Target_ESTest.java')
+        destination_file1 = os.path.join(destination_path, 'Target_ESTest.java')
+        shutil.move(source_file1, destination_file1)
+
+        source_file2 = os.path.join(source_path, 'Target_ESTest_scaffolding.java')
+        destination_file2 = os.path.join(destination_path, 'Target_ESTest_scaffolding.java')
+        shutil.move(source_file2, destination_file2)
